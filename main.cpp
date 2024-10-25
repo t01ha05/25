6bad00d1b8a54c5a05839b0426c1a777b1b97370
Talha Ahmed <t01haa2005@gmail.com>
2024-10-25 14:39:03 -0700

    Initial commit

--- main.cpp
+++ main.cpp
@@ -25,3 +25,23 @@ int main() {
     return 0;
 }
 
+void readVector(vector<string>& vecCodes, long long& timeTaken) {
+    ifstream inFile(FILE_NAME);
+
+    if (!inFile) {
+        cerr << "Error opening file: " << FILE_NAME << endl;
+        exit(1);
+
+    }
+    string code;
+    auto start = high_resolution_clock::now();
+    while (getline(inFile, code)) {
+        vecCodes.push_back(code);
+}
+void sortVector (vector<string>& vecCodes, long long& timeTaken) {
+    auto start = high_resolution___clock::now();
+    sort(vecCodes.rbegin(), vecCodes.rend());
+    auto end = high_resolution_clock::now();
+    timeTaken = duration_cast<milliseconds>(end - start).count();
+}
+
